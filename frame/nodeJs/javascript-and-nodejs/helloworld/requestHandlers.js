eda69801a5cfa3c064fca5b39302e4fa9e822b1e
wy8473979981 <wangyang5739@163.com>
2018-12-01 16:24:02 +0800

    update

--- frame/nodeJs/javascript-and-nodejs/helloworld/requestHandlers.js
+++ frame/nodeJs/javascript-and-nodejs/helloworld/requestHandlers.js
@@ -1,48 +1,66 @@
 var querystring = require("querystring"),
-    fs = require("fs");
+	fs = require("fs"),
+	formidable = require("formidable");
 
-function start(response, postData) {
-  console.log("Request handler 'start' was called.");
+function start(response) {
+	console.log("Request handler 'start' was called.");
 
-  var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" '+
-    'content="text/html; charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
-    '</form>'+
-    '</body>'+
-    '</html>';
+	var body = '<html>' +
+		'<head>' +
+		'<meta http-equiv="Content-Type" content="text/html; ' +
+		'charset=UTF-8" />' +
+		'</head>' +
+		'<body>' +
+		'<form action="/upload" enctype="multipart/form-data" ' +
+		'method="post">' +
+		'<input type="file" name="upload" multiple="multiple">' +
+		'<input type="submit" value="Upload file" />' +
+		'</form>' +
+		'</body>' +
+		'</html>';
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
+	response.writeHead(200, {
+		"Content-Type": "text/html"
+	});
+	response.write(body);
+	response.end();
 }
 
-function upload(response, postData) {
-  console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: "+
-  querystring.parse(postData).text);
-  response.end();
+function upload(response, request) {
+	console.log("Request handler 'upload' was called.");
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+		fs.renameSync(files.upload.path, "C:/Users/wangy/Desktop/git-file/All-daily-documents/frame/nodeJs/javascript-and-nodejs/helloworld/tmp/ppp.png");
+		response.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+		response.write("received image:<br/>");
+		response.write("<img src='/show' />");
+		response.end();
+	});
 }
 
-function show(response, postData) {
-  console.log("Request handler 'show' was called.");
-  fs.readFile("/tmp/test.png", "binary", function(error, file) {
-    if(error) {
-      response.writeHead(500, {"Content-Type": "text/plain"});
-      response.write(error + "\n");
-      response.end();
-    } else {
-      response.writeHead(200, {"Content-Type": "image/png"});
-      response.write(file, "binary");
-      response.end();
-    }
-  });
+function show(response) {
+	console.log("Request handler 'show' was called.");
+	fs.readFile("C:/Users/wangy/Desktop/git-file/All-daily-documents/frame/nodeJs/javascript-and-nodejs/helloworld/tmp/ppp.png", "binary", function(error, file) {
+		if(error) {
+			response.writeHead(500, {
+				"Content-Type": "text/plain"
+			});
+			response.write(error + "/n");
+			response.end();
+		} else {
+			response.writeHead(200, {
+				"Content-Type": "image/png"
+			});
+			debugger
+			response.write(file, "binary");
+			response.end();
+		}
+	});
 }
 
 exports.start = start;
