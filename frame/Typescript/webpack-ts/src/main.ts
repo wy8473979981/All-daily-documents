3100f9fb18a0ea29af3c4d31037e23137c29317e
SH-AD\jack.wang <1101123893@qq.com>
2021-10-28 18:11:40 +0800

    vue3

--- frame/Typescript/webpack-ts/src/main.ts
+++ frame/Typescript/webpack-ts/src/main.ts
@@ -231,24 +231,90 @@ interface StudentInfos {
 
 // type studentName = NonNullable<string | null | undefined> // string
 
+// class Animal {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+//   static isAnimal(instantce: any) {
+//     return instantce instanceof Animal;
+//   }
+//   eat() {
+//     console.log(`${this.name} is eating`);
+//   }
+// }
 
+// const dog = new Animal("旺财");
+// dog.eat(); //旺财 is eating
+// Animal.isAnimal(dog); //true
 
-class Animal {
-  name: string;
+// type boy = { name: string; age: number; mustache: string };
+// type girl = { name: string; age: number };
+// //交叉类型
+// let result1: boy & girl = {
+//   name: "tao",
+//   age: 28,
+//   mustache: "胡子",
+// };
+// console.log(result1.name); // 能.出所有属性
+// //联合类型
+// let result2: boy | girl = {
+//   name: "shen",
+//   age: 18,
+//   mustache: "胡子",
+// };
+// console.log(result2.name); //只能.age和name（共有属性）
+// console.log(result2.age); //只能.age和name（共有属性）
+// console.log(result2.mustache); //只能.age和name（共有属性）
 
-  constructor(name: string) {
-    this.name = name;
-  }
+// tsc index.ts --strictNullChecks  //该命令可以检测null
+//可选参数的处理
+// function fn(x:number, y?:number){
+//     return x + (y || 0)
+// }
+// fn(1, 2)
+// fn(1)
+// fn(1, undefined)
+// // fn(1, null)  //报错
+// //可选属性的处理
+// class C{
+//     a:number,
+//     b?:number
+// }
+// let c = new C()
+// c.a = 12
+// c.a = undefined  //报错
+// c.b = 13
+// c.b = undefined  //可选的默认是undefined
+// c.b = null       //报错，null不能赋值给联合类型或undefined 
+// // 正常情况下，name不会为null
+// // 但是ts编译器过不了，会认为name有可能为null
+// // 因为编译器无法处理嵌套函数里的null，解决方法：name加！号
+// function fn(name:string | null): string {
+//     function postfix(epither:string){
+//         return name!.charAt(0) + '. the' + epither
+//     }
+//     name = name || 'Bob'  
+//     return postfix(name)
+// }
 
-  static isAnimal(instantce: any) {
-    return instantce instanceof Animal;
-  }
+// 写法一：
+// 如果不断言，可能拿不到length，因为有可能传number
+// function getLength(input:number | string):number{
+//     const str = input as String  //断言为string
+//     if(str.length){ //可以使用string的length
+//         return str.length
+//     }else{
+//         const number as Number  //断言为number
+//         return number.toString().length
+//     }
+// }
 
-  eat() {
-    console.log(`${this.name} is eating`);
-  }
-}
-
-const dog = new Animal("旺财");
-dog.eat(); //旺财 is eating
-Animal.isAnimal(dog); //true
+// 写法二：断言写法二
+// function getLength(input:number | string):number{
+//     if((<string>input).length){
+//         return (<string>input).length
+//     }else{
+//         return input.toString().length
+//     }
+// }
