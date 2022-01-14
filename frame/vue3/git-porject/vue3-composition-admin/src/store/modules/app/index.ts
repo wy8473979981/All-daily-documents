119c9e744b6118b7a950645059fe2d933f5335c6
SH-AD\jack.wang <1101123893@qq.com>
2022-01-14 16:27:11 +0800

    ww

--- frame/vue3/git-porject/vue3-composition-admin/src/store/modules/app/index.ts
+++ frame/vue3/git-porject/vue3-composition-admin/src/store/modules/app/index.ts
@@ -23,26 +23,7 @@ import type { AppState } from './state'
 // export type ... from — 一样， 仅用作导出。
 
 export type { AppState } from './state'
-// 用于创建字符串列表映射至 `K: V` 的函数
-function strEnum<T extends string>(o: Array<T>): { [K in T]: K } {
-  return o.reduce((res, key) => {
-    res[key] = key;
-    return res;
-  }, Object.create(null));
-}
 
-// 创建 K: V
-const Direction = strEnum(['North', 'South', 'East', 'West']);
-
-// 创建一个类型
-type Direction = keyof typeof Direction;
-
-// 简单的使用
-let sample: Direction;
-
-sample = Direction.North; // Okay
-sample = 'North'; // Okay
-sample = 'AnythingElse'; // ERROR!
 export type AppStore<S = AppState> = Omit<VuexStore<S>, 'getters' | 'commit' | 'dispatch'>
 & {
   commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
@@ -65,3 +46,4 @@ export const store: Module<AppState, RootState> = {
   // But without it, a bigger store might have clashes in namings
   // namespaced: true,
 }
+
