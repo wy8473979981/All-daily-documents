23cc379dcd665176db708e19b7dc19a48a2f00de
SH-AD\jack.wang <1101123893@qq.com>
2022-01-25 18:10:48 +0800

    2323

--- frame/Typescript/learn/base.js
+++ frame/Typescript/learn/base.js
@@ -1,18 +1,1 @@
-"use strict"
-Object.defineProperty(exports, "__esModule", { value: true })
-exports.createCatName = void 0
-'createCatName'
-cannot
-be
-used
-value
-because
-it
-was
-imported
-using
-'import type'
-
-const createCatName = () => "fluffy"
-exports.createCatName = createCatName
-const name
+"use strict";
